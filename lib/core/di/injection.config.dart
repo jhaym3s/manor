23eb3af949730e018d519974b8555d924853afc8
Jhaym3s <jhaymescode@gmail.com>
2026-07-04 15:13:09 +0100

    di, bloc and firebase fix

--- lib/core/di/injection.config.dart
+++ lib/core/di/injection.config.dart
@@ -9,34 +9,34 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
-import 'package:cloud_firestore/cloud_firestore.dart' as _i974;
-import 'package:firebase_auth/firebase_auth.dart' as _i59;
-import 'package:firebase_messaging/firebase_messaging.dart' as _i892;
-import 'package:firebase_storage/firebase_storage.dart' as _i457;
-import 'package:get_it/get_it.dart' as _i174;
-import 'package:injectable/injectable.dart' as _i526;
-import 'package:manor/core/di/firebase_module.dart' as _i941;
+import 'package:cloud_firestore/cloud_firestore.dart' as cloud;
+import 'package:firebase_auth/firebase_auth.dart' as auth;
+import 'package:firebase_messaging/firebase_messaging.dart' as messaging;
+import 'package:firebase_storage/firebase_storage.dart' as storage;
+import 'package:get_it/get_it.dart' as GI;
+import 'package:injectable/injectable.dart' as inject;
+import 'package:manor/core/di/firebase_module.dart' as module;
 
-extension GetItInjectableX on _i174.GetIt {
+extension GetItInjectableX on GI.GetIt {
   // initializes the registration of main-scope dependencies inside of GetIt
-  _i174.GetIt init({
+  GI.GetIt init({
     String? environment,
-    _i526.EnvironmentFilter? environmentFilter,
+    inject.EnvironmentFilter? environmentFilter,
   }) {
-    final gh = _i526.GetItHelper(this, environment, environmentFilter);
+    final gh = inject.GetItHelper(this, environment, environmentFilter);
     final firebaseModule = _$FirebaseModule();
-    gh.lazySingleton<_i59.FirebaseAuth>(() => firebaseModule.firebaseAuth);
-    gh.lazySingleton<_i974.FirebaseFirestore>(
+    gh.lazySingleton<auth.FirebaseAuth>(() => firebaseModule.firebaseAuth);
+    gh.lazySingleton<cloud.FirebaseFirestore>(
       () => firebaseModule.firebaseFirestore,
     );
-    gh.lazySingleton<_i457.FirebaseStorage>(
+    gh.lazySingleton<storage.FirebaseStorage>(
       () => firebaseModule.firebaseStorage,
     );
-    gh.lazySingleton<_i892.FirebaseMessaging>(
+    gh.lazySingleton<messaging.FirebaseMessaging>(
       () => firebaseModule.firebaseMessaging,
     );
     return this;
   }
 }
 
-class _$FirebaseModule extends _i941.FirebaseModule {}
+class _$FirebaseModule extends module.FirebaseModule {}
